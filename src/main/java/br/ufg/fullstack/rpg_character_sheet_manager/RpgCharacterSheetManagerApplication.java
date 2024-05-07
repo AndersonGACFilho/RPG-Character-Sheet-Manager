d2008c2be190545049c1abafca4daed1412622ca
Anderson <andersonfilho@discente.ufg.br>
2024-05-07 11:59:42 -0300

    Functionality: Adding some fundamental files to the project

---
+++ src/main/java/br/ufg/fullstack/rpg_character_sheet_manager/RpgCharacterSheetManagerApplication.java
@@ -0,0 +1,13 @@
+package br.ufg.fullstack.rpg_character_sheet_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgCharacterSheetManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpgCharacterSheetManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/ufg/fullstack/rpg_character_sheet_manager/RpgCharacterSheetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufg.fullstack.rpg_character_sheet_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgCharacterSheetManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
